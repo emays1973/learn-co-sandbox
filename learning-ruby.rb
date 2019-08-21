0e2d40b6971c44d05e7e7ebe54b4c5afd5412e3e
Eric Mays <emays1973@gmail.com>
2019-08-21 00:31:48 +0000

    Automatically backed up by Learn

---
+++ learning-ruby.rb
@@ -0,0 +1,17 @@
+#run_code_inside = FALSE
+#puts "code before if...end"
+#if run_code_inside
+  #puts "code inside"
+#end
+#puts "Code after if...end"
+
+n = 2 
+count = 0 
+while count <= n do
+  puts "I ran."
+  count = count + 1 
+end
+
+5.times do
+  puts "Eric is a winner!"
+end
